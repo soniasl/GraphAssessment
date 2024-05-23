72f9d8bd54bee7e31b25bfd353ab16fd20b179b1
Sonia Serrano Lorenzo <soniaserranolorenzo@gmail.com>
2024-05-23 16:41:18 -0400

    Added gradient for top area border

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -23,17 +23,27 @@ export default function Graph({ data }) {
     return (
         <AreaChart height={400} width={800} data={chartData}>
             <defs>
-                <linearGradient id="hour" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#33FF8A" stopOpacity={0.8} />
+                <linearGradient id="stroke" x1="0" y1="0" x2="1" y2="0">
+                    <stop offset="50%" stopColor="#F0FF33" stopOpacity={1} />
+                    <stop offset="100%" stopColor="#33FF8A" stopOpacity={1} />
+                </linearGradient>
+                <linearGradient id="area" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="10%" stopColor="#33FF8A" stopOpacity={0.5} />
                     <stop offset="95%" stopColor="#33FF8A" stopOpacity={0} />
                 </linearGradient>
             </defs>
 
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="Day" />
-            <YAxis dataKey="Time" />
+            <YAxis dataKey="Time" type="number" />
             <Tooltip />
-            <Area type="monotone" dataKey="Time" fill="url(#hour)" />
+            <Area
+                type="monotone"
+                dataKey="Time"
+                stroke="url(#stroke)"
+                strokeWidth={8}
+                fill="url(#area)"
+            />
         </AreaChart>
     );
 }
